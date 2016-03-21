ad7535bef420342b8f00d3426439614a869cd57c
Ricardo Henrique <ricardohenrique996@gmail.com>
2016-03-21 19:24:48 -0300

    Adicionado mais comentários esplicativos.

--- HelloWorld/HelloWorld/Form1.cs
+++ HelloWorld/HelloWorld/Form1.cs
@@ -10,6 +10,10 @@ using System.Windows.Forms;
 
 namespace HelloWorld
 {
+    /*É possível dividir a definição de uma class ou uma struct, interface ou
+    metódo em dois ou mais arquivos fonte.Cada arquivo contém uma seção para 
+    definição de tipo ou método, e todas as partes são combinadas quando a 
+    aplicação é compilada.*/
     public partial class Form1 : Form
     {
         public Form1()
